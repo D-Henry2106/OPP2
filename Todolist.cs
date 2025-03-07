ed048ef70ff91f486b93e1e71b70a51de229e76e
lenovo <lenovo@DESKTOP-239I9LJ>
2025-03-07 15:42:26 +0700

    Add project files.

---
+++ Todolist.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPP2
+{
+    internal class Todolist
+    {
+        List<Task> tasks = new List<Task>();
+        public void insertTask(Task task)
+        {
+            tasks.Add(task);
+        }
+        public void displayTasks()
+        {
+            int i = 1;
+            foreach (var task in tasks)
+            {
+                Console.Write(i + ": ");
+                Console.WriteLine(task.getDescription());
+                i++;
+            }
+        }
+    }
+}
